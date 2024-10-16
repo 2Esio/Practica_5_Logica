171097929e334ed72807acdfabfb4470058f0891
2Esio <marco@localhost.localdomain>
2024-10-15 20:26:23 -0600

    Se agrega esqueleto en practica_5.pl | 2Esio | 15-10-2024 20:26

---
+++ practica5.pl
@@ -0,0 +1,17 @@
+%% Ejercicios %%
+ %1. Crea una base de conocimiento e implementa las relaciones de acuerdo a los siguientes eneunciados*/
+    %a) Dos personas viven en la misma casa si tienen la misma direccion
+    %
+    %b) Los que juegan basquetbol o futbol americano son deportistas
+    %
+    %c) Eres britanico si eres gales, ingles, escoces o irlandes
+    %
+    %d) Te pueden multar si no has pagado la tenencia de tu automovil
+    %
+    %e) Una persona puede ser ciudadano mexicano si tiene la nacionalidad mexicana, es mayor edad y es honesto
+    %
+  %2. Implementa una funcion que devuelva el maximo de una lista de numeros, todas las funciones auxiliares deben ser propias. Ademas, incluye como seria la ejecucion de la lista [1, 2, 3, 2, 1,]
+  %
+  %3. Implementa una funcion que devuelva la cantidad de numeros distintos que hay en una lista
+  %
+  %4. Implementa la funcion de Fibonacci. Incluye como seria la ejecucion de Fibonacci con 5.
